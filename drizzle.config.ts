1bb37be848c39845a031de96a777f6123008add0
MK-Mike <mike@axiumeducation.org>
2025-08-01 22:58:53 +0200

    added turso

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,11 +2,21 @@ import { type Config } from "drizzle-kit";
 
 import { env } from "~/env";
 
+// export default {
+//   schema: "./src/server/db/schema.ts",
+//   dialect: "sqlite",
+//   dbCredentials: {
+//     url: env.DATABASE_URL,
+//   },
+//   tablesFilter: ["kent-cook-this_*"],
+// } satisfies Config;
+
 export default {
   schema: "./src/server/db/schema.ts",
-  dialect: "sqlite",
+  dialect: "turso",
   dbCredentials: {
-    url: env.DATABASE_URL,
+    url: env.TURSO_DATABASE_URL,
+    authToken: env.TURSO_AUTH_TOKEN,
   },
   tablesFilter: ["kent-cook-this_*"],
 } satisfies Config;
