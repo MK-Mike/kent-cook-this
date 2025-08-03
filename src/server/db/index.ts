0831b095260fb9190f8e17bcd8372bc9d7445c07
MK-Mike <mike@axiumeducation.org>
2025-08-03 15:00:17 +0200

    updated db client to use local db in dev

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,23 +1,29 @@
 import { createClient, type Client } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
-
 import { env } from "~/env";
 import * as schema from "./schema";
 
-/**
- * Cache the database connection in development. This avoids creating a new connection on every HMR
- * update.
- */
+// Use a global cache for the client in dev (for HMR)
 const globalForDb = globalThis as unknown as {
-  client: Client | undefined;
+  dbClient?: Client;
 };
 
-export const client =
-  globalForDb.client ??
-  createClient({
-    url: env.TURSO_DATABASE_URL,
-    authToken: env.TURSO_AUTH_TOKEN,
-  });
-if (env.NODE_ENV !== "production") globalForDb.client = client;
+function getClient() {
+  if (env.NODE_ENV !== "production") {
+    globalForDb.dbClient ??= createClient({
+      url: "file:kent-cook-this.db",
+      syncUrl: env.TURSO_DATABASE_URL,
+      authToken: env.TURSO_AUTH_TOKEN,
+      syncInterval: 60,
+    });
+    return globalForDb.dbClient;
+  } else {
+    // In production, always create a new client (no global caching)
+    return createClient({
+      url: env.TURSO_DATABASE_URL,
+      authToken: env.TURSO_AUTH_TOKEN,
+    });
+  }
+}
 
-export const db = drizzle(client, { schema });
+export const db = drizzle(getClient(), { schema });
