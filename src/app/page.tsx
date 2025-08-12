db030a98b31767d02f4a55d466bcd5cf22a7f563
MK-Mike <mike@axiumeducation.org>
2025-08-12 20:14:47 +0200

    shadcn init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { LatestPost } from "~/app/_components/post";
 import { Ingredient } from "./_components/ingredient";
 import { api, HydrateClient } from "~/trpc/server";
+import { Button } from "~/components/ui/button";
 
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
